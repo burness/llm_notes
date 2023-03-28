8a7542931309fba5f14916439aa0475e91a7ad20
burnessduan <burness1990@gmail.com>
2023-03-28 23:41:03 +0800

    [feature] add gpt model code

--- gpt/model.py
+++ gpt/model.py
@@ -2,6 +2,7 @@ import math
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
+from utils import CfgNode as CN
 
 
 class NewGELU(nn.Module):
@@ -42,7 +43,7 @@ class CausalSelfAttention(nn.Module):
         return y
     
 
-class Blcok(nn.Module):
+class Block(nn.Module):
 
     def __init__(self, config):
         super().__init__()
@@ -55,3 +56,183 @@ class Blcok(nn.Module):
             act = NewGELU(),
             dropout = nn.Dropout(config.resid_pdrop),
         ))
+        m = self.mlp
+        self.mlpf = lambda x: m.dropout(m.c_proj(m.act(m.c_fc(x))))
+
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlpf(self.ln_2(x))
+        return x
+    
+
+class GPT(nn.Module):
+    @staticmethod
+    def get_default_config():
+        c = CN()
+        c.model_type = "gpt"
+        c.n_layer = None
+        c.n_head = None
+        c.n_embd = None
+
+        c.vocab_size = None
+        c.block_size = None
+        
+        c.embed_pdrop = 0.1
+        c.resid_pdrop = 0.1
+        c.attn_pdrop = 0.1
+
+        return c
+    
+    def __init__(self, config):
+        super.__init__()
+        assert config.vocab_size is not None
+        assert config.block_size is not None
+        self.block_size = config.block_size
+
+        type_given = config.model_type is not None
+        params_given = all([config.n_layer is not None, config.n_head is not None, config.n_embd is not None])
+        assert type_given ^ params_given
+        if type_given:
+            config.merge_from_dict({
+                "open-gpt": dict(n_layer=12, n_head=12, n_embd=768),
+                "gpt-2": dict(n_layer=12, n_head=12, n_embd=768),
+                "gpt2-medium": dict(n_layer=24, n_head=16, n_embd=1024),
+                "gpt2-large": dict(n_layer=36, n_head=20, n_embd=1280),
+                "gpt2-xl": dict(n_layer=48, n_head=25, n_embd=1600),
+                "gopher-44m": dict(n_layer=8, n_head=16, n_embd=512),
+                "gpt-mini": dict(n_layer=6, n_head=6, n_embd=192),
+                "gpt-micro": dict(n_layer=4, n_head=4, n_embd=128),
+                "gpt-nano": dict(n_layer=3, n_head=3, n_embd=48),
+            }[config.model_type])
+        self.transformer = nn.ModuleDict(dict(
+            wte = nn.Embedding(config.vocab_size, config.n_embd),
+            wpe = nn.Embedding(config.block_size, config.n_embd),
+            drop = nn.Dropout(config.embd_pdrop),
+            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+            ln_f = nn.LayerNorm(config.n_embd),
+        ))
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
+
+        self.apply(self._init_weights)
+        for pn, p in self.named_parameters:
+            if pn.endswith("c_proj.weight"):
+                torch.nn.init.normal_(p, mean=0.0, std=0.02/math.sqrt(2 * config.n_layer))
+
+        n_params = sum(p.numel() for p in self.transformer.parameters())
+        print("number of parameters: %.2fM" % (n_params/1e6,))
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding): 
+            # TODO: use the sine/cosine functions to be pos embedding later, the implementation here is 
+            # from https://github.com/huggingface/transformers/blob/main/src/transformers/models/gpt2/modeling_gpt2.py#L462
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+        elif isinstance(module, nn.LayerNorm):
+            torch.nn.init.zeros_(module.bias)
+            torch.nn.init.ones_(module.weight)
+
+    @classmethod
+    def from_pretrained(cls, model_type):
+        assert model_type in {"gpt2", "gpt2-medium", "gpt2-large", "gpt2-xl"}
+        from transformers import GPT2LMHeadModel
+
+        config = cls.get_default_config()
+        config.model_type = model_type
+        config.vocab_size = 50257
+        config.block_size = 1024
+        model = GPT(config)
+        sd = model.state_dict()
+        print("sd: {}".format(sd))
+
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
+        print("sd_hf: {}".format(sd_hf))
+
+        keys = [k for k in sd_hf if not k.endswith("attn.masked_bias")]
+        transposed = ["attn.c_attn.weight", "attn.c_proj.weight", "mlp.c_fc.weight", "mlp.c_proj.weight"]
+
+        assert len(keys) == len(sd)
+        for k in keys:
+            if any(k.endswith(w) for w in transposed):
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
+            else:
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
+        return model
+    
+    def configure_optimizers(self, train_config):
+        decay = set()
+        no_decay = set()
+
+        whitelist_weight_modules = (torch.nn.Linear, )
+        blacklist_weight_modules = (torch.nn.LayerNorm, torch.nn.Embedding)
+        for mn, m in self.named_modules():
+            for pn, p in m.named_parameters:
+                fpn = "%s.%s" %(mn, pn) if mn else pn
+                if pn.endswith("bias"):
+                    no_decay.add(fpn)
+                elif pn.endswith("weight") and isinstance(m, whitelist_weight_modules):
+                    decay.add(fpn)
+                elif pn.endswith("weight") and isinstance(m, blacklist_weight_modules):
+                    no_decay.add(fpn)
+
+        param_dict = {pn: p for pn, p in self.named_parameters()}
+        inter_params = decay & no_decay
+        union_oparams = decay | no_decay
+
+        assert len(inter_params) == 0, "parameters %s mode it into both decay/no_decay sets! " % (str(inter_params), )
+        assert len(param_dict.keys() - union_oparams) == 0, "parameters %s were not separated into either decay/no_decay set! " \
+                                                      % (str(param_dict.keys() - union_oparams), )
+        
+        optim_groups = [
+            {"params": [param_dict[pn] for pn in sorted(list(decay))], "weight_decay": train_config.weight_decay},
+            {"params": [param_dict[pn] for pn in sorted(list(no_decay))], "weight_decay":0.0},
+        ]
+        optimizer = torch.optim.AdamW(optim_groups, lr = train_config.learning_rate, betas = train_config.betas)
+        return optimizer 
+    
+
+    def forward(self, idx, targets=None):
+        device = idx.device
+        b, t = idx.size()
+        assert t <= self.block_size, f"Cannot forward sequence of length {t}, block size is only {self.block_size}"
+        pos = torch.arange(0, t, dtype=torch.long, device=device).unsqueeze(0)
+
+        tok_emb = self.transformer.wte(idx)
+        pos_emb = self.transformer.wpe(pos)
+        x = self.transformer.drop(tok_emb + pos_emb)
+        for block in self.transformer.h:
+            x = block(x)
+        x = self.transformer.ln_f(x)
+        logits = self.lm_head(x)
+        loss = None
+
+        if targets is not None:
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1), ignore_index=-1)
+        
+        return logits, loss
+    
+
+    @torch.no_grad()
+    def generate(self, idx, max_new_tokens, temperature=1.0, do_sample=False, top_k=None):
+        for _ in range(max_new_tokens):
+            idx_cond = idx if idx.size(1) <= self.block_size else idx[:, -self.block_size:]
+            logits, _ = self(idx_cond)
+            logits = logits[:, -1, :] / temperature
+            if top_k is not None:
+                v, _ = torch.topk(logits, top_k)
+                logits[logits < v[:, [-1]]] = -float("Inf")
+            probs = F.softmax(logits, dim=-1)
+            if do_sample:
+                idx_next = torch.multinomial(probs, num_samples=1)
+            else:
+                _, idx_next = torch.topk(probs, k=1, dim=-1)
+            idx = torch.cat((idx, idx_next), dim=1)
+        return idx
+
--- gpt/utils.py
+++ gpt/utils.py
@@ -1,5 +1,5 @@
 import os
-
+from ast import literal_eval
 class CfgNode:
     def __init__(self, **kwargs):
         self.__dict__.update(kwargs)
@@ -18,4 +18,32 @@ class CfgNode:
         parts = [" " *(indent * 4) + p for p in parts]
         return "".join(parts)
     
+    def to_dict(self):
+        return {k: v.to_dict() if istance(v, CfgNode) else v for k, v in self.__dict__.items()}
     
+    def merge_from_dict(self, d):
+        self.__dict__.update(d)
+
+    def merge_from_args(self, args):
+        for arg in args:
+            keyval = arg.split("=")
+            assert len(keyval) == 2, "expecting each overide arg to be of form --arg=value, got %s" % arg
+            key, val = keyval
+
+            try:
+                val = literal_eval(val)
+            except ValueError:
+                pass
+
+            assert key[:2] == "--"
+            key = key[2:]
+            keys = key.split('.')
+            obj = self
+            for k in keys[:-1]:
+                obj = getattr(obj, k)
+            leaf_key = key[-1]
+
+            assert hasattr(obj, leaf_key), f"{key} is not an attribute that exists in the config"
+
+            print("command line overwriting config attribte %s with %s" % (key, val))
+            setattr(obj, leaf_key, val)
